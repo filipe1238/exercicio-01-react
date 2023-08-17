f516947c9a2a5b9234417e0c75853ed4e3e05d9b
filipe <filipe.correa@sonner.com.br>
2023-08-17 12:09:40 -0300

    atualizacao para rotas

---
+++ src/Pages/SingleWallpaper.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function SingleWallpaper() {
+    //get id from uri
+    let baseUrl = "https://picsum.photos/v2/list";
+    const id = window.location.pathname.split('/')[2]
+
+  return (
+    <div>SingleWallpaper</div>
+  )
+}
+
+export default SingleWallpaper
